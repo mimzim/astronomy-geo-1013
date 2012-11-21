e442c221c95ca57f71a9a5c50681fe427ce4c15c
BeckiD75@gmail.com <BeckiD75@gmail.com@b98e7108-8b99-11fe-5032-7c00b827e427>
2012-11-21 05:18:49 +0000

    started submission code

--- scripts/quiz1.js
+++ scripts/quiz1.js
@@ -1,17 +1,71 @@
 // This function processes the quiz data
 function submitQuiz1()
 {
+	var numberCorrect = 0;
 	var q1 = document.forms.quiz1.question1;
 	var q2 = document.forms.quiz1.question2;
+	var q3 = document.forms.quiz1.question3;
+	var q4 = document.forms.quiz1.question4;
+	var q5 = document.forms.quiz1.question5;
+	var q6 = document.forms.quiz1.question6;
+	var q7 = document.forms.quiz1.question7;
+	var q8 = document.forms.quiz1.question8;
+	var q9 = document.forms.quiz1.question9;
+	var q10 = document.forms.quiz1.question10;
+	var q11 = document.forms.quiz1.question11;
+	var q12 = document.forms.quiz1.question12;
+	var q13 = document.forms.quiz1.question13;
+	var q14 = document.forms.quiz1.question14;
+	var q15 = document.forms.quiz1.question15;
+	var q16 = document.forms.quiz1.question16;
+	var q17 = document.forms.quiz1.question17;
+	var q18 = document.forms.quiz1.question18;
+	var q19 = document.forms.quiz1.question19;
+	var q20 = document.forms.quiz1.question20;
 	
-	if( q1[ 0 ].checked == true && q2[ 2 ].checked == true )
+	
+	if( q1[2].checked == true)
 	{
-		alert( "You passed!" );
+		numberCorrect = numberCorrect + 1;
 	}
-	else
-	{
-		alert( "you failed" );
-	}
+	
+	if( q2[0].checked == true)
+		{
+			numberCorrect = numberCorrect + 1;
+		}
+	
+	if(q3[2].checked == true)
+		{
+			numberCorrect = numberCorrect + 1;
+		}
+	
+	if(q4[1].checked == true)
+		{
+			numberCorrect = numberCorrect + 1;
+		}
+	
+	if(q5[1].checked == true)
+		{
+			numberCorrect = numberCorrect + 1;
+		}
+	if(q6[0].checked == true)
+		{
+			numberCorrect = numberCorrect + 1;
+		}
+
+	var score = numberCorrect / 20 * 100;
+	alert("Your score is " + score + "%");
+	
+	if(score >= 70)
+		{
+			alert("You passed!")
+			document.getElementById("certificate").style.display = "inline";
+		}
+	
+	if(score < 70)
+		{
+			alert("Try again.")
+		}
 	
 	// call the function to report scores
 	parent.reportScores();
