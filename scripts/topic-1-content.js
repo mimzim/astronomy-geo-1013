e3a260ac46a8b59cc6d098bc14f17ea1ff08bd8e
ddancer@satx.rr.com <ddancer@satx.rr.com@b98e7108-8b99-11fe-5032-7c00b827e427>
2012-11-22 14:45:25 +0000

    created spacing and breaks in astronomer information, q/a.

--- scripts/topic-1-content.js
+++ scripts/topic-1-content.js
@@ -12,13 +12,14 @@ function printTychoBrahe()
 {    
     document.getElementById( "as" ).src = "images/Brahe.jpg";
     document.getElementById( "title" ).innerHTML = "Tycho Brahe (1546-1601 Danish, Protestant North)";
-    document.getElementById( "box" ).innerHTML ="Given an island by the King of Denmark to build an observatory with sophisticated equipment for measuring the distance between stars.<br/>" +
-    		"Left Behind 20 years of observations. <br>" +
-    		"Data on Mar's orbit that was crucial for Kelper to construct our present model of the solar system.<br/>" +
-    		"Used parallax to show:<br/> Either earth is the center of the solar system<br/>" +
+    document.getElementById( "box" ).innerHTML ="Given an island by the King of Denmark to build an observatory with sophisticated equipment for measuring the distance between stars.<br/><br/>" +
+    		". Left Behind 20 years of observations. <br>" +
+    		". Data on Mar's orbit that was crucial for Kelper to construct our present model of the solar system.<br/><br/>" +
+    		". Used parallax to show:<br/> " +
+    		"Either earth is the center of the solar system<br/>" +
     		"OR<br/>" +
-    		"the stars or so far away that the parallax was too far to measure.<br/> " +
-    		"Believed in a blend between Ptolemic and Copernicun models (planets orbiting the sun and sun orbits the Earth.";
+    		"The stars or so far away that the parallax was too far to measure.<br/><br/> " +
+    		".  Believed in a blend between Ptolemic and Copernicun models (planets orbiting the sun and sun orbits the Earth.";
 }
 
 
@@ -26,47 +27,40 @@ function printNicolausCopernicus()
 {
     document.getElementById( "as" ).src = "images/Copernicu.jpg";
     document.getElementById( "title" ).innerHTML = "Nicolaus Copernicus (1473-1543 Polish Priest)";
-    document.getElementById( "box" ).innerHTML ="Calculations used to revise Julian calendar to Gregorian claendar.<br/>" +
-    		"Copernicus had a conceptual breakthrough: the Sun centered solar system.<br/>" +
-    		"Still thought circular orbits were correct (perfection in design).<br/>" +
-    		"Wrote De Revolusionibus in 1543, received well by the church, but in 1616<br/>" +
-    		"the book was banned until 1865 due to Protestant reformation.<br/>";
+    document.getElementById( "box" ).innerHTML =". Calculations used to revise Julian calendar to Gregorian claendar.<br/><br/>" +
+    		". Copernicus had a conceptual breakthrough: the Sun centered solar system.<br/><br/>" +
+    		". Still thought circular orbits were correct (perfection in design).<br/><br/>" +
+    		". Wrote De Revolusionibus in 1543, received well by the church, but in 1616 the book was banned until 1865, due to the Protestant reformation.";
 }
 
 
 function printGalileoGalilei()
 
- {
-
-   document.getElementById( "as" ).src = "images/Galileo.jpg";
-        
-   document.getElementById( "title" ).innerHTML = "Galileo Galilei (1564-1642; Italian, anti-reformation Italy)";
-   document.getElementById( "box" ).innerHTML ="Built a telescope after hearing of one built in the Netherlands <br/>" +
-   		"Demonstrated some celestial objects did not revolve around the Earth<br/>" +
-   		"Discovered moons of Jupiter called the Galilean Moons: Io, Europa, Ganymede, Callisto<br/>" +
-   		"Showed phases of Venus as perspective changes over time, more support for a heliocentric solar system<br/>" +
-   		"Galileo's technological breakthroughs gave evidence for the heliocentric model.<br/>" +
-   		"Mountains on the moon: Not a perfect celestial sphere and the moon is similar to Earth.<br/>" +
-   		"Makes idea that the moon revolves around us and everything else revolves around the sun probable.<br/>" +
-   		"Sunspots: not a heavenly, incorruptible body.";
-   		
+{
+	document.getElementById( "as" ).src = "images/Galileo.jpg";
+	document.getElementById( "title" ).innerHTML = "Galileo Galilei (1564-1642; Italian, anti-reformation Italy)";
+	document.getElementById( "box" ).innerHTML =". Built a telescope after hearing of one built in the Netherlands <br/><br/>" +
+	   		". Demonstrated some celestial objects did not revolve around the Earth<br/><br/>" +
+	   		". Discovered moons of Jupiter called the Galilean Moons: Io, Europa, Ganymede, Callisto<br/><br/>" +
+	   		". Showed phases of Venus as perspective changes over time, more support for a heliocentric solar system<br/><br/>" +
+	   		". Galileo's technological breakthroughs gave evidence for the heliocentric model.<br/><br/>" +
+	   		". Mountains on the moon: Not a perfect celestial sphere and the moon is similar to Earth.<br/><br/>" +
+	   		". Makes idea that the moon revolves around us and everything else revolves around the sun probable.<br/><br/>" +
+	   		". Sunspots: not a heavenly, incorruptible body.";
 }
 
        
 function printJohannesKepler()
 {
-
- document.getElementById( "as" ).src = "images/Kepler.jpg";
-
- document.getElementById( "title" ).innerHTML = "Johannes Kepler (1571-1630 German, Protestant north)";
- 
- document.getElementById( "box" ).innerHTML ="Thought about the solar system in 3 dimensions and that the spacing between the sphere on which " +
- 		"the planets moved were determined by perfect geometric shapes. <br/>" +
- 		"Kepler's laws of planetary motion<br/>"+
- 		"Kepler quote: 'My aim is to show that the heavenly machine is not a kind of divine, live being, but a kind of clockwork, insofar as" +
- 		"nearly all the manifold motions are caused by a most simple, magnetic, and material force," +
- 		"just as all motions of the clock are caused by a simple weight. And I also show how these" +
- 		"physical causes are to be given numerical and geometrical expression' (Gaithers, 2007)";
+	 document.getElementById( "as" ).src = "images/Kepler.jpg";
+	 document.getElementById( "title" ).innerHTML = "Johannes Kepler (1571-1630 German, Protestant north)";
+	 document.getElementById( "box" ).innerHTML =". Thought about the solar system in 3 dimensions and that the spacing between the sphere on which " +
+	 		"the planets moved were determined by perfect geometric shapes.<br/><br/>" +
+	 		". Kepler's laws of planetary motion<br/><br/>"+
+	 		". Kepler quote: 'My aim is to show that the heavenly machine is not a kind of divine, live being, but a kind of clockwork, insofar as" +
+	 		"nearly all the manifold motions are caused by a most simple, magnetic, and material force," +
+	 		"just as all motions of the clock are caused by a simple weight. And I also show how these" +
+	 		"physical causes are to be given numerical and geometrical expression' (Gaithers, 2007)";
  }
  
 function printClaudiusPtolemy()
@@ -74,29 +68,27 @@ function printClaudiusPtolemy()
     document.getElementById( "as" ).src = "images/Ptolemaeus.jpg";
     document.getElementById( "title" ).innerHTML = "Claudius Ptolemy (165 AD, Greek)";
     document.getElementById( "box" ).innerHTML ="THe Ptolmec solar system (accepted for 1500 years)<br/>" +
-    		"Earth centered- Celectial objects move at constant speed and move in circular orbits<br/>" +
-    		"Planents have epicycles that account for observed periodic irregularities in planetary motions like planets apearing to change directions." +
-    		"Developed Quadrants to be used for astronomical measurements, notably determining the altitude of celestial objects" +
-    		"";
-    		
+    		". Earth centered- Celectial objects move at constant speed and move in circular orbits<br/>" +
+    		". Planets have epicycles that account for observed periodic irregularities in planetary motions like planets apearing to change directions." +
+    		". Developed Quadrants to be used for astronomical measurements, notably determining the altitude of celestial objects";
 } 
 
 function printSirIssacNewton()
 {    
     document.getElementById( "as" ).src = "images/Newton.jpg";
     document.getElementById( "title" ).innerHTML = "Sir Issac Newton (1642-1717, England)";
-    document.getElementById( "box" ).innerHTML ="Edmond Halley (1656-1742) of Halley's comet, visited him concerned about Kepler's Law of elliptical orbit.<br/>"+
-    	"Newton's Law of gravity explained elliptical orbits and speed of orbits.<br/>";
+    document.getElementById( "box" ).innerHTML =". Collaborated with Edmond Halley for whom Halley's comet was named.<br/><br/>" +
+    	". They revisited Kepler's Law of elliptical orbit.<br/><br/>"+
+    	". Newton's Law of gravity explained elliptical orbits and speed of orbits.<br/>";
 }
 
 function printAristotle()
 {    
     document.getElementById( "as" ).src = "images/aristotle.jpg";
-    document.getElementById( "title" ).innerHTML = "Aristotle";
-    document.getElementById( "box" ).innerHTML ="Aristotle (considered the Father of Western Science) was born in 384 BC in Macedonia. Sent to Plato's Academy in Athens." +
-    		"when he was 17 years old. Tutored Alexander the Great when he was a child. Believed in the 4 basic elements; earth, water, air, fire with each" +
-    		"element having a tendency to go up or down. This was the 1st idea of gravity, before anyone ever knew what gravity was. Used parallax to show that Earth did not move." +
-    		"";
+    document.getElementById( "title" ).innerHTML = "Aristotle (384 BCE, Macedonia)";
+    document.getElementById( "box" ).innerHTML =". Believed in the 4 basic elements; earth, water, air, fire with each element having a tendency to go up or down<br/><br/>. " +
+    		". This was the 1st idea of gravity, before anyone ever knew what gravity was<br/><br/>. " +
+    		". Used parallax to show that Earth did not move.";
 }
 
 
