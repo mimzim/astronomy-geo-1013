dcb6b1acb3ecbf3cb371a56086f3757927e23c95
BeckiD75@gmail.com <BeckiD75@gmail.com@b98e7108-8b99-11fe-5032-7c00b827e427>
2012-11-29 19:03:51 +0000

    QA - space added in Kepler info on astronomers page

--- scripts/topic-1-content.js
+++ scripts/topic-1-content.js
@@ -58,7 +58,7 @@ function printJohannesKepler()
 	 		"the planets moved were determined by perfect geometric shapes.<br/><br/>" +
 	 		". Kepler's laws of planetary motion.<br/><br/>"+
 	 		". Kepler quote: 'My aim is to show that the heavenly machine is not a kind of divine, live being, but a kind of clockwork, insofar as" +
-	 		"nearly all the manifold motions are caused by a most simple, magnetic, and material force," +
+	 		" nearly all the manifold motions are caused by a most simple, magnetic, and material force," +
 	 		" just as all motions of the clock are caused by a simple weight. And I also show how these" +
 	 		"physical causes are to be given numerical and geometrical expression.' (Gaithers, 2007).";
  }
