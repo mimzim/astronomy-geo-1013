91fb6833afb25ee15f5679b2552e13fb007584ff
BeckiD75@gmail.com <BeckiD75@gmail.com@b98e7108-8b99-11fe-5032-7c00b827e427>
2012-12-08 04:05:07 +0000

    Added alt and title text to images called via JS

--- trunk/scripts/topic-1-content.js
+++ trunk/scripts/topic-1-content.js
@@ -1,9 +1,13 @@
 
 function printTychoBrahe()
 {    
-    document.getElementById( "as" ).src = "images/brahe.jpg";
-    document.getElementById( "title" ).innerHTML = "Tycho Brahe<br/>(1546-1601 Danish, Protestant North)";
-    document.getElementById( "box" ).innerHTML ="Given an island by the King of Denmark to build an observatory with sophisticated equipment for measuring the distance between stars.<br/><br/>" +
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Tycho Brahe");
+    t.setAttribute("alt", "Tycho Brahe");
+    t.setAttribute("src", "images/brahe.jpg");
+	document.getElementById( "title" ).innerHTML = "Tycho Brahe<br/>(1546-1601 Danish, Protestant North)";
+	document.getElementById( "box" ).innerHTML ="Given an island by the King of Denmark to build an observatory with sophisticated equipment for measuring the distance between stars.<br/><br/>" +
     		". Left Behind 20 years of observations. <br/><br/>" +
     		". Data on Mar's orbit that was crucial for Kelper to construct our present model of the solar system.<br/><br/>" +
     		". Used parallax to show:<br/><br/> " +
@@ -16,7 +20,11 @@ function printTychoBrahe()
 
 function printNicolausCopernicus()
 {
-    document.getElementById( "as" ).src = "images/copernicus.jpg";
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Nicolaus Copernicus");
+    t.setAttribute("alt", "Nicolaus Copernicus");
+    t.setAttribute("src", "images/copernicus.jpg");
     document.getElementById( "title" ).innerHTML = "Nicolaus Copernicus<br/>(1473-1543 Polish Priest)";
     document.getElementById( "box" ).innerHTML =". Calculations used to revise Julian calendar to Gregorian calendar.<br/><br/>" +
     		". Copernicus had a conceptual breakthrough: the Sun centered solar system.<br/><br/>" +
@@ -28,8 +36,12 @@ function printNicolausCopernicus()
 function printGalileoGalilei()
 
 {
-	document.getElementById( "as" ).src = "images/galileo.jpg";
-	document.getElementById( "title" ).innerHTML = "Galileo Galilei<br/>(1564-1642; Italian, anti-reformation Italy)";
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Galileo Galilei");
+    t.setAttribute("alt", "Galileo Galilei");
+    t.setAttribute("src", "images/galileo.jpg");
+    document.getElementById( "title" ).innerHTML = "Galileo Galilei<br/>(1564-1642; Italian, anti-reformation Italy)";
 	document.getElementById( "box" ).innerHTML =". Built a telescope after hearing of one built in the Netherlands. <br/><br/>" +
 	   		". Demonstrated some celestial objects did not revolve around the Earth.<br/><br/>" +
 	   		". Discovered moons of Jupiter called the Galilean Moons: Io, Europa, Ganymede, Callisto.<br/><br/>" +
@@ -43,9 +55,13 @@ function printGalileoGalilei()
        
 function printJohannesKepler()
 {
-	 document.getElementById( "as" ).src = "images/kepler.png";
-	 document.getElementById( "title" ).innerHTML = "Johannes Kepler<br/>(1571-1630 German, Protestant north)";
-	 document.getElementById( "box" ).innerHTML =". Thought about the solar system in 3 dimensions and that the spacing between the sphere on which " +
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Johannes Kepler");
+    t.setAttribute("alt", "Johanes Kepler");
+    t.setAttribute("src", "images/kepler.png");
+	document.getElementById( "title" ).innerHTML = "Johannes Kepler<br/>(1571-1630 German, Protestant north)";
+	document.getElementById( "box" ).innerHTML =". Thought about the solar system in 3 dimensions and that the spacing between the sphere on which " +
 	 		"the planets moved were determined by perfect geometric shapes.<br/><br/>" +
 	 		". Kepler's laws of planetary motion.<br/><br/>"+
 	 		". Kepler quote: 'My aim is to show that the heavenly machine is not a kind of divine, live being, but a kind of clockwork, insofar as" +
@@ -56,9 +72,13 @@ function printJohannesKepler()
  
 function printClaudiusPtolemy()
 {
-    document.getElementById( "as" ).src = "images/ptolemaeus.jpg";
-    document.getElementById( "title" ).innerHTML = "Claudius Ptolemy<br/>(165 AD, Greek)";
-    document.getElementById( "box" ).innerHTML =". The Ptolmec solar system (accepted for 1500 years).<br/><br/>" +
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Claudius Ptolemy");
+    t.setAttribute("alt", "Claudius Ptolemy");
+    t.setAttribute("src", "images/ptolemaeus.jpg");
+	document.getElementById( "title" ).innerHTML = "Claudius Ptolemy<br/>(165 AD, Greek)";
+	document.getElementById( "box" ).innerHTML =". The Ptolmec solar system (accepted for 1500 years).<br/><br/>" +
     		". Earth centered- Celectial objects move at constant speed and move in circular orbits.<br/><br/>" +
     		". Planets have epicycles that account for observed periodic irregularities in planetary motions like planets apearing to change directions.<br/><br/>" +
     		". Developed Quadrants to be used for astronomical measurements, notably determining the altitude of celestial objects.";
@@ -66,18 +86,26 @@ function printClaudiusPtolemy()
 
 function printSirIssacNewton()
 {    
-    document.getElementById( "as" ).src = "images/newton.jpg";
-    document.getElementById( "title" ).innerHTML = "Sir Issac Newton<br/>(1642-1717, England)";
-    document.getElementById( "box" ).innerHTML =". Collaborated with Edmond Halley for whom Halley's comet was named.<br/><br/>" +
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Sir Isaac Newton");
+    t.setAttribute("alt", "Sir Isaac Newton");
+    t.setAttribute("src", "images/newton.jpg");
+	document.getElementById( "title" ).innerHTML = "Sir Issac Newton<br/>(1642-1717, England)";
+	document.getElementById( "box" ).innerHTML =". Collaborated with Edmond Halley for whom Halley's comet was named.<br/><br/>" +
     	". They revisited Kepler's Law of elliptical orbit.<br/><br/>"+
     	". Newton's Law of gravity explained elliptical orbits and speed of orbits.<br/>";
 }
 
 function printAristotle()
 {    
-    document.getElementById( "as" ).src = "images/aristotle.jpg";
-    document.getElementById( "title" ).innerHTML = "Aristotle<br/>(384 BCE, Macedonia)";
-    document.getElementById( "box" ).innerHTML =". Believed in the 4 basic elements; earth, water, air, fire with each element having a tendency to go up or down.<br/><br/>" +
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Aristotle");
+    t.setAttribute("alt", "Aristotle");
+    t.setAttribute("src", "images/aristotle.jpg");
+	document.getElementById( "title" ).innerHTML = "Aristotle<br/>(384 BCE, Macedonia)";
+	document.getElementById( "box" ).innerHTML =". Believed in the 4 basic elements; earth, water, air, fire with each element having a tendency to go up or down.<br/><br/>" +
     		". This was the 1st idea of gravity, before anyone ever knew what gravity was.<br/><br/>" +
     		". Used parallax to show that Earth did not move.";
 }
@@ -85,7 +113,11 @@ function printAristotle()
 
 function printGeocentricity()
 {    
-    document.getElementById( "as" ).src = "images/geocentric.jpg";
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Geocentricity");
+    t.setAttribute("alt", "Geocentricity");
+    t.setAttribute("src", "images/geocentric.jpg");
     document.getElementById( "title" ).innerHTML = "Geocentricity";
     document.getElementById( "box" ).innerHTML =". Geocentricity, a model developed by Ptolemy, was the belief that the Earth is the center of the universe.<br/><br/>" +
     		". Geocentric thinking also held Earth was fixed in space, not moving, and was special and unique compared to " +
@@ -94,7 +126,11 @@ function printGeocentricity()
 
 function printRetrogradeMotion()
 {    
-    document.getElementById( "as" ).src = "images/retrograde.jpg";
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Retrograde Motion");
+    t.setAttribute("alt", "Retrograde Motion");
+    t.setAttribute("src", "images/retrograde.jpg");
     document.getElementById( "title" ).innerHTML = "Retrograde Motion";
     document.getElementById( "box" ).innerHTML = ".  Retrograde Motion is movement in the opposite direction to the movement of another object.<br/><br/>" +
     		".  An example of retrograde motion is the Earth rotating one direction around the sun and a satellite rotating" +
@@ -106,7 +142,11 @@ function printRetrogradeMotion()
 
 function printStellarParallax()
 {    
-    document.getElementById( "as" ).src = "images/stellar-paralax.png";
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Stellar Parallax");
+    t.setAttribute("alt", "Stellar Parallax");
+    t.setAttribute("src", "images/stellar-paralax.png");
     document.getElementById( "title" ).innerHTML = "Stellar Parallax";
     document.getElementById( "box" ).innerHTML ="Stellar parallax:<br/><br/>" +
     		"1. Either earth is at the center of the solar system<br/><br/>" +
@@ -116,9 +156,14 @@ function printStellarParallax()
 
 function printKeplersLaws()
 {    
-    document.getElementById( "as" ).src = "images/kepler-law-3.png";
-    document.getElementById( "title" ).innerHTML = "Kepler's Laws";
-    document.getElementById( "box" ).innerHTML ="Kepler's Laws:<br/><br/>" +
+    var t = document.getElementById("as");
+    
+    t.setAttribute("title", "Kepler's Third Law");
+    t.setAttribute("alt", "Kepler's Third Law");
+    t.setAttribute("src", "images/kepler-law-3.png");
+	document.getElementById( "as" ).src = "images/kepler-law-3.png";
+	document.getElementById( "title" ).innerHTML = "Kepler's Laws";
+	document.getElementById( "box" ).innerHTML ="Kepler's Laws:<br/><br/>" +
     		"1. Planets have elliptical orbits, with the sun at one focus.<br/><br/>" +
     		"2. Line joining the planet to the sun sweets out equal areas in equal times as the planet travels around the ellipse." +
     		"(The planets move faster when closer to the sun.)<br/><br/>" +
